6599cd0fd6e293760d46a8096d4f2b8c1d19317b
mbalcer <mateusz75319@gmail.com>
2020-04-26 15:42:23 +0200

    Fixed camera controller and jumping error

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,18 +6,21 @@ public class CameraController : MonoBehaviour
 {
     private Transform player;
     private Vector3 cameraPos;
-    private int vertBound = 13;
-    private int horBound = 5;
-    private int rightBound;
-    private int leftBound;
-    private int topBound;
-    private int bottomBound;
+    private float vertBound;
+    private float horBound;
+    private float rightBound;
+    private float leftBound;
+    private float topBound;
+    private float bottomBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        rightBound = vertBound;
-        leftBound = -vertBound;
+        Camera cam = Camera.main;
+        horBound = 2f * cam.orthographicSize;
+        vertBound = horBound * cam.aspect;
+        rightBound = vertBound/2;
+        leftBound = -vertBound/2;
         topBound = horBound;
         bottomBound = -horBound;
         player = GameObject.FindWithTag("Player").transform;
@@ -29,30 +32,30 @@ public class CameraController : MonoBehaviour
     {
         if(player.position.x >= rightBound)
         {
-            cameraPos.x += vertBound*2;
-            rightBound += vertBound * 2;
-            leftBound += vertBound * 2;
+            cameraPos.x += vertBound;
+            rightBound += vertBound;
+            leftBound += vertBound;
             transform.position = cameraPos;
         }
         else if(player.position.x <= leftBound)
         {
-            cameraPos.x -= vertBound * 2;
-            rightBound -= vertBound * 2;
-            leftBound -= vertBound * 2;
+            cameraPos.x -= vertBound;
+            rightBound -= vertBound;
+            leftBound -= vertBound;
             transform.position = cameraPos;
         }
         else if(player.position.y >= topBound)
         {
-            cameraPos.y += horBound*2;
-            topBound += horBound*2;
-            bottomBound += horBound * 2;
+            cameraPos.y += horBound;
+            topBound += horBound;
+            bottomBound += horBound;
             transform.position = cameraPos;
         }
         else if(player.position.y <= bottomBound)
         {
-            cameraPos.y -= horBound * 2;
-            topBound -= horBound * 2;
-            bottomBound -= horBound * 2;
+            cameraPos.y -= horBound;
+            topBound -= horBound;
+            bottomBound -= horBound;
             transform.position = cameraPos;
         }
     }
