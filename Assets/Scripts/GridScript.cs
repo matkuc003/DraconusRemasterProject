44f7f42cc5742c70c202e56d421f4080e804f6ae
mbalcer <mateusz75319@gmail.com>
2020-04-27 12:34:02 +0200

    Created grid script to visible hidden wall

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridScript : MonoBehaviour
+{
+    private bool visibleHiddenWall = false;
+    public Tilemap tilemap;
+
+    public void setVisibleHiddenWall(bool visible)
+    {
+        visibleHiddenWall = visible;
+        changeVisibleWall();
+    }
+
+    void changeVisibleWall()
+    {
+        if (visibleHiddenWall)
+        {
+            Vector3Int currentCell = tilemap.WorldToCell(transform.position);
+            for(int i=82; i<=85; i++)
+            {
+                for(int j=-11; j>=-13; j--)
+                {
+                    currentCell.x = i;
+                    currentCell.y = j;
+                    tilemap.SetTile(currentCell, null);
+                }
+            }
+        }
+    }
+}
