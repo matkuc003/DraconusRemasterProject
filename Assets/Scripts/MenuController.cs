d7f322ac75d380b9b558f4c6eab3a4df536876ca
matkuc003 <mnorris.k97@gmail.com>
2020-05-09 17:12:12 +0200

    Added menu scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onPlay()
+    {
+        Application.LoadLevel("GameScene");
+    }
+    public void onExit()
+    {
+        Application.Quit();
+    }
+}
