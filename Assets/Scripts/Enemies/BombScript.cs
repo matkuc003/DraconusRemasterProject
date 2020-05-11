dc7a413c810a427ed432984726d9d9f3fca11716
mbalcer <mateusz75319@gmail.com>
2020-05-11 11:06:10 +0200

    Added bombs

---
+++ Assets/Scripts/Enemies/BombScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    public bool direction; // false - down, true - up
+    public float maxDistance;
+    public float timeRest;
+    private float timeRest2 = 0;
+    private Vector2 startPosition;
+    void Start()
+    {
+        startPosition = this.gameObject.transform.position;
+
+        if (!direction)
+            startPosition.y -= maxDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRest2 > 0)
+        {
+            timeRest2 -= Time.deltaTime;
+            Debug.Log(timeRest2);
+        }
+        else
+        {
+            Debug.Log("else");
+            Vector2 actualPosition = gameObject.transform.position;
+            if (direction)
+                actualPosition.y += 0.1f;
+            else
+                actualPosition.y -= 0.1f;
+
+            this.gameObject.transform.position = actualPosition;
+
+            Debug.Log(startPosition.y + maxDistance + ", " + actualPosition.y);
+            if (startPosition.y >= actualPosition.y || startPosition.y + maxDistance <= actualPosition.y)
+            {
+                direction = !direction;
+                timeRest2 = timeRest;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 0.8f;
+
+    private float turnTimer;
+    public float timeTrigger;
+
+    private Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        turnTimer = 0;
+        timeTrigger = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector3(myRigidbody.transform.localScale.x * speed, myRigidbody.velocity.y, 0f);
+
+        turnTimer += Time.deltaTime;
+        if (turnTimer >= timeTrigger)
+        {
+            turnAround();
+            turnTimer = 0;
+        }
+    }
+    void turnAround()
+    {
+        if (transform.localScale.x == 0.5f)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Fire")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
