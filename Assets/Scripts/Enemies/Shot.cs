2b74dd1b38f3a89dc172a6c67596df025ad3a8be
mbalcer <mateusz75319@gmail.com>
2020-05-13 00:50:43 +0200

    Added gun who shoots pills

---
+++ Assets/Scripts/Enemies/Shot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 actualPosition = gameObject.transform.position;
+        actualPosition.y -= 0.1f;
+        gameObject.transform.position = actualPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Base" || other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
